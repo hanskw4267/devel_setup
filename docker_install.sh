fd3754e09aa54776ba7749024e340d7898228053
hanskw4267 <hanskurniaw@gmail.com>
2021-09-16 09:26:57 +0800

    removed user prompt

--- docker_install.sh
+++ docker_install.sh
@@ -27,14 +27,14 @@ accept_all
 
 print_msg 'Installing docker' 
 sudo apt remove docker docker-engine docker.io containerd runc
-sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 docker run hello-world
 
 if ask_user "Do you wish to install docker compose??" ; then
