4e8b72eb8c2b1016d2c107369afbe119fde0672f
hanskw4267 <hanskurniaw@gmail.com>
2022-05-25 16:50:15 +0800

    ros: added -y flag for curl install

--- ros_install.sh
+++ ros_install.sh
@@ -76,7 +76,7 @@ echo "-------------------------------------------------"
 
 echo " <-- Setting up apt servers --> "
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl # if you haven't already installed curl
+sudo apt install -y curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install -y ros-"${ROS_DISTRO}"-"${ROS_CONFIG}"
